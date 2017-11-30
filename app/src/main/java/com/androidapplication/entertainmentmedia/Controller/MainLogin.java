e7bd96fa6249134d1f0eb12e082718131c30549d
Zachary Painter <zacharypainter@knights.ucf.edu>
2017-11-30 13:56:12 -0500

    Login API start

--- app/src/main/java/com/androidapplication/entertainmentmedia/Controller/MainLogin.java
+++ app/src/main/java/com/androidapplication/entertainmentmedia/Controller/MainLogin.java
@@ -2,11 +2,13 @@ package com.androidapplication.entertainmentmedia.Controller;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,9 +17,24 @@ import android.widget.Toast;
 
 import com.androidapplication.entertainmentmedia.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainLogin extends AppCompatActivity {
 
     private int counter;
+    private static final String TASKS_URL = "https://cop4331.herokuapp.com/api/sessions";
 
     private TextView attemptsCounter;
     private Button loginButton;
@@ -62,20 +79,89 @@ public class MainLogin extends AppCompatActivity {
 
     private void loginUser(String username, String password)
     {
-        if(username.equals("admin") && password.equals("admin")) {
-            Toast.makeText(getApplicationContext(),
-                    "Redirecting...",Toast.LENGTH_LONG).show();
-            finish();
-        }else{
-            Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_LONG).show();
+        if (username.isEmpty() || password.isEmpty())
+            return;
 
-            attemptsCounter.setBackgroundColor(Color.RED);
-            counter--;
-            attemptsCounter.setText(Integer.toString(counter));
+        //loginButton.setEnabled(false);
 
-            if (counter == 0) {
-                registerButton.setEnabled(false);
+        //Put user credentials into object for API call
+        Map<String, String> postData = new HashMap<>();
+        postData.put("username", username);
+        postData.put("password", password);
+
+        authTask task = new authTask(postData);
+        task.execute();
+
+    }
+
+    private class authTask extends AsyncTask<String, Void, Void> {
+
+        JSONObject postData;
+        JSONObject responseData;
+
+        public authTask(Map<String, String> postData) {
+            if (postData != null) {
+                this.postData = new JSONObject(postData);
             }
+        }
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            try
+            {
+                URL url = new URL("https://cop4331.herokuapp.com/api/sessions");
+                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+
+                connection.setDoOutput(true);
+                connection.setDoInput(true);
+                connection.setRequestMethod("POST");
+
+                //Header type
+                connection.setRequestProperty("Content-Type", "application/json");
+
+                //Put the JSON object into message body
+                if (this.postData != null) {
+                    OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+                    writer.write(postData.toString());
+                    writer.flush();
+                }
+
+                //Don't know if this does anything
+                //connection.connect();
+
+                //Get response
+                int responseCode = connection.getResponseCode();
+
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+
+                    InputStream stream = new BufferedInputStream(connection.getInputStream());
+
+                    BufferedReader streamReader = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+                    StringBuilder responseStrBuilder = new StringBuilder();
+
+                    String inputStr;
+
+                    //Convert InputStream from response to JSONObject
+                    try {
+                        while ((inputStr = streamReader.readLine()) != null)
+                            responseStrBuilder.append(inputStr);
+                        responseData = new JSONObject(responseStrBuilder.toString());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    Log.d("Reponse", String.valueOf(responseCode));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        protected void onPostExecute()
+        {
+
         }
     }
 
